4e85267ddc01df69d7bb2f7e396297c7ed673dd6
thuya soe <thuyasoe1215@gmai.com>
2023-02-21 08:44:57 +0630

    add post and get api

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-import { config } from "dotenv"
-config()
+import { config } from "dotenv";
+config();
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import DeckModel from "./models/Deck";
 
 const port = 3000;
 const app = express();
-app.use(express.json())
+
+app.use(cors());
+app.use(express.json());
 
 mongoose.set("strictQuery", true);
 
@@ -14,17 +17,20 @@ app.get("/", (req: Request, res: Response) => {
   res.send("hello word");
 });
 
+app.get("/decks", async (req: Request, res: Response) => {
+  const decks = await DeckModel.find();
+  res.json(decks);
+});
+
 app.post("/decks", async (req: Request, res: Response) => {
   const newDeck = new DeckModel({
-    title: req.body.title
+    title: req.body.title,
   });
   const createdDeck = await newDeck.save();
   res.json(createdDeck);
-})
+});
 
-mongoose
-  .connect(process.env.MONGO_URL!)
-  .then(() => {
-    console.log(`listening on port ${port}`);
-    app.listen(port);
-  });
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  console.log(`listening on port ${port}`);
+  app.listen(port);
+});
